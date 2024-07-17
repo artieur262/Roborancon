c84ea53e1633f3d09ff7d4f49942f7f9bcf3e68f
artieur262 <130496437+artieur262@users.noreply.github.com>
2024-07-17 11:40:37 +0200

    otipmisation de mémoire et ajout de propreté

--- interface/start_menu.py
+++ interface/start_menu.py
@@ -11,6 +11,7 @@ from interface.graphique import (
     screen,
     place_texte_in_texture,
     vider_affichage,
+    gener_texture,
 )
 from interface.bouton import Bouton
 
@@ -159,3 +160,38 @@ class StartMenu:
         self.afficher()
         pygame.display.update()
         return temp
+
+    @staticmethod
+    def main(clavier, souris, langue: str):
+        """fonction principale"""
+        taille_bouton = (200, 50)
+        texture_bouton = [
+            gener_texture(taille_bouton, (175, 175, 175)) for _ in range(2)
+        ]
+        texture_bouton[0].blit(
+            gener_texture(
+                (taille_bouton[0] - 10, taille_bouton[1] - 10), (150, 150, 150)
+            ),
+            (5, 5),
+        )
+        texture_bouton[1].blit(
+            gener_texture(
+                (taille_bouton[0] - 10, taille_bouton[1] - 10), (100, 100, 100)
+            ),
+            (5, 5),
+        )
+        fond = gener_texture((150, 150), (200, 200, 200))
+        fond = r".git\IMG_4840.jpg"
+        start_menu = StartMenu(fond, texture_bouton, taille_bouton, langue)
+        encours = True
+        start_menu.reset()
+
+        clock = pygame.time.Clock()
+        while encours:
+
+            temp = start_menu.play(clavier, souris)
+            if "jouer" == temp or start_menu.button_langue[langue][0] == temp:
+                return "jouer"
+            if "quitter" == temp or start_menu.button_langue[langue][2] == temp:
+                return "quitter"
+            clock.tick(90)
--- main.py
+++ main.py
@@ -9,34 +9,21 @@ from interface.class_clavier import Clavier, Souris
 def main():
     """fonction principale"""
     langue = "fr"
-    taille_bouton = (200, 50)
-    texture_bouton = [gener_texture(taille_bouton, (175, 175, 175)) for _ in range(2)]
-    texture_bouton[0].blit(
-        gener_texture((taille_bouton[0] - 10, taille_bouton[1] - 10), (150, 150, 150)),
-        (5, 5),
-    )
-    texture_bouton[1].blit(
-        gener_texture((taille_bouton[0] - 10, taille_bouton[1] - 10), (100, 100, 100)),
-        (5, 5),
-    )
-    fond = gener_texture((150, 150), (200, 200, 200))
-    fond = r".git\IMG_4840.jpg"
-    start_menu = StartMenu(fond, texture_bouton, taille_bouton, langue)
     clavier = Clavier()
     souris = Souris()
-
-    encours = True
-    start_menu.reset()
-
     clock = pygame.time.Clock()
+    encours = True
+    action = "menu_start"
     while encours:
-
-        temp = start_menu.play(clavier, souris)
-        if "jouer" == temp or start_menu.button_langue[langue][0] == temp:
+        if action == "menu_start":
+            temp = StartMenu.main(clavier, souris, langue)
+            if temp == "quitter":
+                encours = False
+            if temp == "jouer":
+                action = "jeu"
+        if action == "jeu":
             teste.main()
-        if "quitter" == temp or start_menu.button_langue[langue][2] == temp:
-            encours = False
-        clock.tick(90)
+            action = "menu_start"
 
 
 if __name__ == "__main__":
