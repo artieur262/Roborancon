c30067f143a23bf477086bb07f4d3c5ab621dcce
Artieur262 <raguinmateo@gmail.com>
2024-09-21 10:03:22 +0200

    déplacement de acien teste et renomage

--- teste7.py
+++ teste7.py
@@ -1,23 +1,27 @@
 import pygame
 from interface.graphique import screen
 from interface.actualisation_pygame import actualise_event
-from game.entity.playeur import Playeur
-from game.inventaire.item import Item, Membre, MembreSens, Corps
-from game.map import Mur, Porte
+
+from game.entity.playeur import Playeur 
 from game.interface.game import Game
+from game.map import Mur, Porte
+
+# from game.inventaire.item import Item, Membre, MembreSens, Corps
+
 from autre import save
 
 # pylint: disable=no-member
 
-def main():
+def main(): # pylint: disable=missing-function-docstring
     porte=Porte((100,100),(100,100),["textures/porte/porte 1 pos1.png"
         ,"textures/porte/porte 1 pos2.png"])
-    # list_porte=[Porte((100*i1,100*i2),(100,100),["textures/porte/porte 1 pos1.png"
-    #     ,"textures/porte/porte 1 pos2.png"])for i1 in range(10) for i2 in range(10)]
-    # list_mur=[Mur((100*i1,100*i2),(100,100),"textures/mur/mur1.png")for i1 in range(10) for i2 in range(10)]  
+    # list_porte=[Porte((100*i1,100*i2),(100,100),["textures/porte/porte 1 pos1.png",
+    #     "textures/porte/porte 1 pos2.png"])for i1 in range(10) for i2 in range(10)]
+    # list_mur=[Mur((100*i1,100*i2),(100,100),"textures/mur/mur1.png")
+    #           for i1 in range(10) for i2 in range(10)]
     mur=Mur((100,300),(100,100),"textures/mur/mur1.png")
     playeur =Playeur.genere_self(save.load_json("teste/playeur.json"))
-    control = save.load_json("option/control.json")[0]
+    control = save.load_json("option/control.json")[0]     
     game=Game(control, playeur, [])
     game.porte.append(porte)
     game.mur.append(mur)
